cf35b9e7123c68d28e64a5b817b0c25dfae281c7
SVJayanthi <sravanvjayanthi@gmail.com>
2020-05-17 20:57:41 -0400

    Update project.py

--- project.py
+++ project.py
@@ -11,7 +11,7 @@ from ase.calculators.espresso import Espresso
 from ase.constraints import UnitCellFilter
 from ase.optimize import LBFGS
 
-"""
+
 pseudopotentials = {'Na': 'Na_pbe_v1.uspp.F.UPF',
                     'Cl': 'Cl.pbe-n-rrkjus_psl.1.0.0.UPF'}
 rocksalt = bulk('NaCl', crystalstructure='rocksalt', a=6.0)
@@ -26,18 +26,3 @@ opt.run(fmax=0.005)
 
 # cubic lattic constant
 print((8*rocksalt.get_volume()/len(rocksalt))**(1.0/3.0))
-"""
-
-H2O = molecule('H2O')
-
-H2O.set_calculator(Espresso())
-dyn = QuasiNewton(H2O)
-dyn.run(fmax=0.05)
-
-E_H2O = H2O.get_potential_energy()
-
-
-
-E_rxn = 2*E_H2 + E_O2 - 2E_H2O
-print(E_rxn) # answer is in eV
-"""
